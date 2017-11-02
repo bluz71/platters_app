63e55bf3ac136ddc04dd0efb11e6c00ad833daae
Dennis B <bluz71@users.noreply.github.com>
2017-11-02 15:43:52 +1100

    First cut talking to Rails API server using Axios.
    Hits CORS issue at the moment.

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,6 +1,29 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Artists = () => <h2>Artists Page</h2>;
+const ARTISTS_URL = 'http://localhost:3000/artists.json';
+
+class Artists extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      artists: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(ARTISTS_URL).then(response => {
+      console.log(response);
+    });
+  }
+
+  render() {
+    return (
+      <h2>Artists Page</h2>
+    );
+  }
+}
 
 export default Artists;
