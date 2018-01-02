a3caffc4c336555c995c62c802eb32d7e3d403e2
Dennis B <bluz71@users.noreply.github.com>
2018-01-02 15:29:42 +1100

    Added in a link to a handy documentation in regards to Jest + snapshot testing.

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../App';
 
+// Snapshot testing with Jest details:
+//   https://hackernoon.com/testing-react-components-with-jest-and-enzyme-41d592c174f
 it('renders without crashing', () => {
   shallow(<App />);
 });
