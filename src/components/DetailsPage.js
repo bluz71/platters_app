293022787e0c8afeb589cda1a3f7ff0cbaa20f27
Dennis B <bluz71@users.noreply.github.com>
2018-01-04 15:31:43 +1100

    Fixed target="_blank" security issue by adding in: rel="noopener noreferrer"

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -11,13 +11,13 @@ const DetailsPage = () => {
           <ul>
             <li>
               <h3>
-                <a href="https://reactjs.org" target="_blank">React</a> <small>version 16</small>
+                <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a> <small>version 16</small>
               </h3>
               <p>A JavaScript library for building user interfaces.</p>
             </li>
             <li>
               <h3>
-                <a href="https://github.com/facebookincubator/create-react-app" target="_blank">Create React App</a>
+                <a href="https://github.com/facebookincubator/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</a>
               </h3>
               <p>Creates React apps with no build configuration.</p>
             </li>
@@ -28,31 +28,31 @@ const DetailsPage = () => {
           <ul>
             <li>
               <h3>
-                <a href="https://github.com/axios/axios" target="_blank">axios</a>
+                <a href="https://github.com/axios/axios" target="_blank" rel="noopener noreferrer">axios</a>
               </h3>
               <p>Promise based HTTP client for the browser and node.js.</p>
             </li>
             <li>
               <h3>
-                <a href="http://numeraljs.com" target="_blank">Numeral.js</a>
+                <a href="http://numeraljs.com" target="_blank" rel="noopener noreferrer">Numeral.js</a>
               </h3>
               <p>A javascript library for formatting and manipulating numbers.</p>
             </li>
             <li>
               <h3>
-                <a href="https://github.com/blakeembrey/pluralize" target="_blank">Pluralize</a>
+                <a href="https://github.com/blakeembrey/pluralize" target="_blank" rel="noopener noreferrer">Pluralize</a>
               </h3>
               <p>Pluralize and singularize words.</p>
             </li>
             <li>
               <h3>
-                <a href="https://react-bootstrap.github.io" target="_blank">React-Bootstrap</a>
+                <a href="https://react-bootstrap.github.io" target="_blank" rel="noopener noreferrer">React-Bootstrap</a>
               </h3>
               <p>The Bootstrap framework, a mobile first front-end library, rebuilt for React.</p>
             </li>
             <li>
               <h3>
-                <a href="https://reacttraining.com/react-router" target="_blank">React Router</a>
+                <a href="https://reacttraining.com/react-router" target="_blank" rel="noopener noreferrer">React Router</a>
               </h3>
               <p>A declarative routing library for React applications.</p>
             </li>
@@ -63,7 +63,7 @@ const DetailsPage = () => {
           <h2 class="page-header">Deployment</h2>
           <ul>
             <li>
-              <h3><a href="https://surge.sh" target="_blank">Surge</a></h3>
+              <h3><a href="https://surge.sh" target="_blank" rel="noopener noreferrer">Surge</a></h3>
               <p>
                 A <em>content delivery network</em> (CDN) provider for front-end developers
                 who need to deploy and host static site projects.
@@ -71,7 +71,7 @@ const DetailsPage = () => {
             </li>
             <li>
               <h3>
-                <a href="https://github.com/sintaxi/surge" target="_blank">Surge CLI</a>
+                <a href="https://github.com/sintaxi/surge" target="_blank" rel="noopener noreferrer">Surge CLI</a>
               </h3>
               <p>The command line interface to the Surge service.</p>
             </li>
@@ -81,14 +81,14 @@ const DetailsPage = () => {
           <h2 class="page-header">Development</h2>
           <ul>
             <li>
-              <h3><a href="https://travis-ci.org/bluz71/platters" target="_blank">Travis CI</a></h3>
+              <h3><a href="https://travis-ci.org/bluz71/platters" target="_blank" rel="noopener noreferrer">Travis CI</a></h3>
               <p>
                 Travis CI is a continuous integration service used to build and test
                 software projects hosted at GitHub.
               </p>
             </li>
             <li>
-              <h3><a href="https://eslint.org" target="_blank">eslint</a></h3>
+              <h3><a href="https://eslint.org" target="_blank" rel="noopener noreferrer">eslint</a></h3>
               <p>The pluggable linting utility for JavaScript and JSX</p>
             </li>
           </ul>
@@ -98,13 +98,13 @@ const DetailsPage = () => {
           <ul>
             <li>
               <h3>
-                <a href="https://facebook.github.io/jest" target="_blank">Jest</a>
+                <a href="https://facebook.github.io/jest" target="_blank" rel="noopener noreferrer">Jest</a>
               </h3>
               <p>A testing framework for JavaScript.</p>
             </li>
             <li>
               <h3>
-                <a href="http://airbnb.io/enzyme" target="_blank">Enzyme</a>
+                <a href="http://airbnb.io/enzyme" target="_blank" rel="noopener noreferrer">Enzyme</a>
               </h3>
               <p>
                 Enzyme is a JavaScript Testing utility for React that makes it
@@ -114,7 +114,7 @@ const DetailsPage = () => {
             </li>
             <li>
               <h3>
-                <a href="https://github.com/blainekasten/enzyme-matchers" target="_blank">jest-enzyme</a>
+                <a href="https://github.com/blainekasten/enzyme-matchers" target="_blank" rel="noopener noreferrer">jest-enzyme</a>
               </h3>
               <p>An assertion library for the Enzyme testing utility.</p>
             </li>
@@ -124,7 +124,7 @@ const DetailsPage = () => {
           <h2 class="page-header">Miscellaneous application features</h2>
           <ul>
             <li>
-              <h3><a href="https://bluz71.github.io/2017/12/22/browser-navigation-in-react-components.html" target="_blank">Browser navigation with React components</a></h3>
+              <h3><a href="https://bluz71.github.io/2017/12/22/browser-navigation-in-react-components.html" target="_blank" rel="noopener noreferrer">Browser navigation with React components</a></h3>
               <p>
                 The main content React components use HTML5 <em>pushState</em>
                 such that browser navigation correctly transistions through
@@ -132,6 +132,16 @@ const DetailsPage = () => {
               </p>
             </li>
           </ul>
+          <br />
+
+          <h2 class="page-header">Logo</h2>
+          <p>
+            Record player icon, made by&nbsp;
+            <a href="http://www.flaticon.com/authors/freepik" target="_blank" rel="noopener noreferrer" class="standout">Freepik</a> from&nbsp;
+            <a href="http://www.flaticon.com" target="_blank" rel="noopener noreferrer" class="standout">www.flaticon.com</a>, is licensed by&nbsp;
+            <a href="https://creativecommons.org/licenses/by/3.0/" target="_blank" rel="noopener noreferrer" class="standout">CC 3.0 BY</a>
+          </p>
+          <br />
         </div>
       </Col>
     </Row>
