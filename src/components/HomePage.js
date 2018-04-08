eb09924ba3796347cf0708ac2cac4eedf20d2c16
Dennis B <bluz71@users.noreply.github.com>
2018-04-08 15:50:20 +1000

    Setting up infrastructure for the home-page.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { API_HOST } from '../config';
 
-const HomePage = () => <h1>Home Page</h1>;
+const HOME_ENDPOINT = `${API_HOST}/home.json`;
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      albumOfTheDay: {},
+      mostRecentAlbums: [],
+      mostRecentComments: []
+    };
+  }
+
+  componentDidMount() {
+    this.getHome();
+  }
+
+  getHome() {
+    axios.get(HOME_ENDPOINT)
+      .then(response => {
+        this.setState({
+          albumOfTheDay: response.data.album_of_the_day,
+          mostRecentAlbums: response.data.most_recent.albums,
+          mostRecentComments: response.data.most_recent.comments
+        });
+      });
+  }
+
+  render() {
+    return (
+      <h1>Home Page</h1>
+    );
+  }
+}
 
 export default HomePage;
