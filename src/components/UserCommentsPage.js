1b409475c4329327d0290e3a146d260741ae533d
Dennis B <bluz71@users.noreply.github.com>
2018-05-09 15:49:03 +1000

    First-cut of endless page scrolling for User comments.

--- src/components/UserCommentsPage.js
+++ src/components/UserCommentsPage.js
@@ -13,18 +13,54 @@ class UserCommentsPage extends Component {
     super(props);
 
     this.user_slug         = props.match.params.id;
-    this.user              = props.location.state.user;
-    this.comments_endpoint = `${API_HOST}/comments/${props.match.params.id}.json`;
+    this.comments_endpoint = `${API_HOST}/comments/${this.user_slug}.json`;
     this.loaded            = false;
+    if (props.location.state) {
+      this.user = props.location.state.user || this.user_slug;
+    }
+    else {
+      this.user = this.user_slug;
+    }
 
     this.state = {
       comments: [],
       pagination: {}
     };
+
+    // Bind 'this' for callback functions.
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentDidMount() {
+    window.onscroll = this.handleScroll;
     this.getComments();
+  }
+
+  componentWillUnmount() {
+    window.onscroll = null;
+  }
+
+  handleScroll() {
+    // See: http://blog.sodhanalibrary.com/2016/08/detect-when-user-scrolls-to-bottom-of.html
+
+    const windowHeight = 'innerHeight' in window ? window.innerHeight : document.documentElement.offsetHeight;
+    const body         = document.body;
+    const html         = document.documentElement;
+    const docHeight    = Math.max(body.scrollHeight, body.offsetHeight,
+                                  html.clientHeight,  html.scrollHeight,
+                                  html.offsetHeight) - 20;
+    const windowBottom = windowHeight + window.pageYOffset;
+    const moreToScroll = this.state.pagination && this.state.pagination.next_page;
+
+    if (moreToScroll && windowBottom >= docHeight) {
+      // Disable scroll handling whilst records are being retrieved.
+      window.onscroll = null;
+      this.comments_endpoint
+        = `${API_HOST}/comments/${this.user_slug}.json?page=${this.state.pagination.next_page}`;
+      this.loaded = false;
+      this.forceUpdate();
+      this.getComments();
+    }
   }
 
   getComments() {
@@ -34,8 +70,13 @@ class UserCommentsPage extends Component {
           this.loaded = true;
           NProgress.done();
         }
+        if (!window.onscroll) {
+          // Re-enable scroll handling now that the records have been
+          // retrieved.
+          window.onscroll = this.handleScroll;
+        }
         this.setState({
-          comments: response.data.comments,
+          comments: [...this.state.comments, ...response.data.comments],
           pagination: response.data.pagination,
         });
       });
