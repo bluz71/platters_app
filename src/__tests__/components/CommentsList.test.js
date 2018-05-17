67fdff1c09bb902476f20e631579829e4f37366a
Dennis B <bluz71@users.noreply.github.com>
2018-05-17 13:54:15 +1000

    New tests and added PropTypes to CommentList component.

---
+++ src/__tests__/components/CommentsList.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CommentsList from '../../components/CommentsList';
+
+const comments = () => {
+  return [
+    {
+      'id': 1,
+      'body': 'First comment',
+      'created_at': '<time datetime=\"2018-05-09T04:05:00Z\" data-local=\"time-ago\">May  9, 2018  4:05am</time>',
+      'user_name': 'joe',
+      'user_slug': 'joe',
+      'gravatar_url': 'https://gravatar.com/avatar/88884e5cb90c02031f031bc4760fcf69?s=80&r=pg&d=identicon',
+      'for': 'Album',
+      'artist': 'ABC',
+      'name': 'ABC',
+      'path': 'artist/abc/album/abc'
+    },
+    {
+      'id': 1,
+      'body': 'Second comment',
+      'created_at': '<time datetime=\"2018-05-05T05:34:56Z\" data-local=\"time-ago\">May  5, 2018  5:34am</time>',
+      'user_name': 'joe',
+      'user_slug': 'joe',
+      'gravatar_url': 'https://gravatar.com/avatar/88884e5cb90c02031f031bc4760fcf69?s=80&r=pg&d=identicon',
+      'for': 'Album',
+      'artist': 'DEF',
+      'name': 'DEF',
+      'path': 'artist/def/album/def'
+    }
+  ];
+};
+
+describe('<CommentsList />', () => {
+  it('renders without crashing', () => {
+    shallow(<CommentsList comments={[]} />);
+  });
+
+  it('renders a list of comments', () => {
+    const wrapper = shallow(<CommentsList comments={comments()} />);
+    expect(wrapper).toMatchSnapshot();
+  })
+});
--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import linkify from 'linkify-lite';
 import simpleFormat from '../helpers/simpleFormat';
@@ -40,6 +41,10 @@ const CommentsList = ({ comments }) => {
       {renderComments(comments)}
     </div>
   );
+};
+
+CommentsList.propTypes = {
+  comments: PropTypes.array
 };
 
 export default CommentsList;
