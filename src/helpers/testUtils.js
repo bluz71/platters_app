e49dc72154c25c2e7c7ef36b7ccc51e6fa0e1e9e
Dennis B <bluz71@users.noreply.github.com>
2018-09-30 16:07:02 +1000

    Fix broken tests due to expired JWT token.
    Now using a very long lived token for the tests.

--- src/helpers/testUtils.js
+++ src/helpers/testUtils.js
@@ -6,9 +6,9 @@ import { appAuth } from '../lib/appAuth';
 const flushPromises = () => new Promise(resolve => setImmediate(resolve));
 
 const logInUser = () => (
-  appAuth.logIn('eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjo1MywibmFtZSI6ImRlbm5pc2I1NSI\
-sImFkbWluIjpmYWxzZSwiZXhwIjoxNTM3ODQ4NDA0LCJpc3MiOiJwbGF0dGVycyIsImF1ZCI6InBs\
-YXR0ZXJzX2FwcCJ9.ZUV69wWVXfiiMtQ2ZFPvgqvhZWwQ4WC1bI15Q9DpFCU')
+  appAuth.logIn('eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoxLCJuYW1lIjoiZnJlZCIsImFkbW\
+luIjpmYWxzZSwiZXhwIjo3NTg2Mjg2OTU3LCJpc3MiOiJwbGF0dGVycyIsImF1ZCI6InBsYXR0ZX\
+JzX2FwcCJ9.GgXqJen-gKGNoF6b3nUxNGn077jQYoS6aIh1QmagHdk')
 );
 
 const logOutUser = () => (
