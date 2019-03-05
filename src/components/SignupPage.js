f84e160c80a1415ee3817cafb120ade169993819
Dennis B <bluz71@users.noreply.github.com>
2019-03-05 16:07:33 +1100

    Initial sign-up hook to the back-end API

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import {
   Row,
   Col,
@@ -11,6 +12,11 @@ import {
   HelpBlock,
   Button
 } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+import { API_HOST, APPLICATION_HOST } from '../config';
+import { toastAlert, toastNotice } from '../helpers/toastMessage';
+
+const USER_NEW_ENDPOINT = `${API_HOST}/api/users`;
 
 class SignupPage extends Component {
   constructor(props) {
@@ -19,8 +25,8 @@ class SignupPage extends Component {
     document.title = 'Platters App';
 
     this.state = {
-      signUpButtonText: 'Submit',
-      nameErrors: []
+      submitButtonText: 'Submit',
+      errors: []
     };
 
     // Bind 'this' for callback functions.
@@ -31,17 +37,59 @@ class SignupPage extends Component {
 
   handleAccountSubmit(event) {
     event.preventDefault();
+
+    this.setState({
+      submitButtonText: (
+        <div>
+          <FontAwesome name="spinner" spin pulse /> Submitting...
+        </div>
+      )
+    });
+
+    const user = {
+      user: {
+        email: this.emailInput.value,
+        password: this.passwordInput.value,
+        name: this.nameInput.value,
+        application_host: APPLICATION_HOST
+      }
+    };
+
+    this.postUser(user);
   }
 
   handleNameBlur() {
     const name = this.nameInput.value;
     if (name.length < 4 || name.length > 20) {
       this.setState({
-        nameErrors: ['Account name must be between 4 and 20 characters long']
+        errors: ['Account name must be between 4 and 20 characters long']
       });
     } else {
-      this.setState({ nameErrors: [] });
+      this.setState({ errors: [] });
     }
+  }
+
+  postUser(user) {
+    axios
+      .post(USER_NEW_ENDPOINT, user)
+      .then((response) => {
+        this.setState({ submitButtonText: 'Submit' });
+        toastNotice(
+          `Hello ${
+            user.name
+          }, in order to complete your sign up, please follow the instructions in the email that was just sent to you. Please check your junk folder if you can not find the email.`
+        );
+      })
+      .catch((error) => {
+        this.setState({ submitButtonText: 'Submit' });
+        // if (error.response && error.response.status === 404) {
+        //   toastAlert(
+        //     'Invalid email address, no user with that email address is registered'
+        //   );
+        // } else {
+        toastAlert('Server error, please try again later');
+        // }
+      });
   }
 
   handleNameFocus() {
@@ -53,7 +101,7 @@ class SignupPage extends Component {
       return;
     }
 
-    return this.state.nameErrors.map((error, index) => (
+    return this.state.errors.map((error, index) => (
       <li key={index} className="list-group-item list-group-item-danger">
         {error}
       </li>
@@ -62,7 +110,7 @@ class SignupPage extends Component {
 
   hasNameErrors() {
     if (
-      this.state.nameErrors.length > 0 &&
+      this.state.errors.length > 0 &&
       this.nameInput !== document.activeElement
     ) {
       return 'has-error';
@@ -128,7 +176,7 @@ class SignupPage extends Component {
                 bsSize="small"
                 className="submit"
               >
-                {this.state.signUpButtonText}
+                {this.state.submitButtonText}
               </Button>
             </Col>
           </FormGroup>
